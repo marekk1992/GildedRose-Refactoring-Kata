80382e56f87d550363510241829b3f4903e68a53
marek1992 <m.komaisko@gmail.com>
2022-11-05 21:03:40 +0200

    write test cases for GildedRose class

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,54 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    public void Sulfuras_Quality_Never_Changes() {
+        // given
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 2, 80)};
+        GildedRose gildedRose = new GildedRose(items);
+
+        // when
+        gildedRose.updateQuality();
+
+        // then
+        assertEquals(80, gildedRose.items[0].quality);
     }
 
+    @Test
+    public void Sulfuras_SellIn_Value_Never_Changes() {
+        // given
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 2, 80)};
+        GildedRose gildedRose = new GildedRose(items);
+
+        // when
+        gildedRose.updateQuality();
+
+        // then
+        assertEquals(2, gildedRose.items[0].sellIn);
+    }
+
+    @Test
+    public void Backstage_Passes_Quality_Drops_To_Zero_After_Concert() {
+        // given
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 0, 30)};
+        GildedRose gildedRose = new GildedRose(items);
+
+        // when
+        gildedRose.updateQuality();
+
+        // then
+        assertEquals(0, gildedRose.items[0].quality);
+    }
+
+    @Test
+    public void Item_Quality_Is_Never_More_Than_50() {
+        // given
+        Item[] items = new Item[]{new Item("Aged Brie", 5, 50)};
+        GildedRose gildedRose = new GildedRose(items);
+
+        // when
+        gildedRose.updateQuality();
+
+        // then
+        assertEquals(50, gildedRose.items[0].quality);
+    }
 }
